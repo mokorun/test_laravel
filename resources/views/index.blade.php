c3f5ecaf15642120b5e57a4fcc33d4f101ceaff0
futo <futo@core-tech.tw>
2020-11-10 11:12:23 +0800

    Hello world from blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>test page top</title>
+</head>
+<body>
+<h2>
+    Hello world from blade!!
+</h2>
+<p>{{date("Y/m/d H:i:s")}}</p>
+</body>
+</html>
